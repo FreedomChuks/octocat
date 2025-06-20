4bdc62f85cabfe29df1d1bd3193e679c1accddd1
freedom.chuks7@gmail.com <freedom.chuks7@gmail.com>
2025-06-20 11:08:09 -0400

    Refactor: Update Kotlin and Compose versions, add Hilt and other dependencies
    This commit updates the Kotlin version from 2.0.21 to 2.1.20 and the Compose BOM version. It also introduces several new dependencies and plugins, including:
    
    - Hilt for dependency injection
    - Kotlinx Serialization for JSON processing
    - KSP for annotation processing
    - Coil for image loading
    - Retrofit for networking
    - Turbine for testing Flows

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -33,11 +33,12 @@ android {
 }
 
 dependencies {
+//    api(projects.core.common)
+//    api(projects.core.model)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.coil.kt)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
 }
